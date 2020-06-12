1490b935a149020e5025c4f25388f98366b49979
Karthik <karthikkumarreddy548@gmail.com>
2020-06-12 11:58:46 +0530

    Made Gradebook classes as public

--- GradeBook/GradeBooks/StandardGardeBook.cs
+++ GradeBook/GradeBooks/StandardGardeBook.cs
@@ -5,7 +5,7 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGardeBook : BaseGradeBook
+    public class StandardGardeBook : BaseGradeBook
     {
         public StandardGardeBook(string name) : base(name)
         {
