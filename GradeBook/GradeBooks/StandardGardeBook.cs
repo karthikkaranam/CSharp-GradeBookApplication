35531026cf614c64c7e9f00281900933a5ad9bd1
Karthik <karthikkumarreddy548@gmail.com>
2020-06-12 11:54:34 +0530

    Added New Grade Type Classes

---
+++ GradeBook/GradeBooks/StandardGardeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGardeBook : BaseGradeBook
+    {
+        public StandardGardeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
